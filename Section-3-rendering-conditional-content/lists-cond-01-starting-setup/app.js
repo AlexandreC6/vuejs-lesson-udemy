30143bc10bbd8f59a0e4d1303fda1965c7d21fcf
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-04-19 11:27:10 +0200

    v-if, v-esle

--- Section-3-rendering-conditional-content/lists-cond-01-starting-setup/app.js
+++ Section-3-rendering-conditional-content/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      inputValueGoal: '',
+      goals: [],
+    };
   },
+  methods: {
+    addGoal () {
+      this.goals.push(this.inputValueGoal);
+    },
+  }
 });
 
 app.mount('#user-goals');
