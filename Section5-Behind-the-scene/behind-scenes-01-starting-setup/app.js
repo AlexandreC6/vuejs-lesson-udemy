b6fa0bbdf223613d5f951bee5008f833075fd22c
Alexandre Camilo <alex.camilo6@hotmail.ch>
2022-05-18 18:51:31 +0200

    done

--- Section5-Behind-the-scene/behind-scenes-01-starting-setup/app.js
+++ Section5-Behind-the-scene/behind-scenes-01-starting-setup/app.js
@@ -6,13 +6,37 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
+    // saveInput(event) {
+    //   this.currentUserInput = event.target.value;
+    // },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+
+      // this.$refs.aaaa.value = event.target.value
+      this.message = this.$refs.userText.value
     },
   },
+  // Avant de charger Vue
+  beforeCreate() {
+    console.log('beforeCreate()');
+  },
+  created(){
+    console.log('created');
+  },
+  // Juste avant de construire l'app Vue
+  beforeMount(){
+    console.log('beforeMounted');
+  },
+  // l'app VueJS déjà construite, template déjà charger
+  mounted(){
+    console.log('mounted');
+  },
+  beforeUpdate(){
+    console.log('beforeUpdate');
+  },
+  updated(){
+    console.log('update');
+  }
 });
 
 app.mount('#app');
